133161305f1cb52eb4e5524912fce7ed0690b56e
tyage <namatyage@gmail.com>
2015-10-30 22:51:23 +0900

    extend public ASTBase

--- src/ast/translation_unit.hpp
+++ src/ast/translation_unit.hpp
@@ -1,7 +1,7 @@
 #ifndef KMC_C89_COMPILER_AST_TRANSLATION_UNIT_HPP
 #define KMC_C89_COMPILER_AST_TRANSLATION_UNIT_HPP
 
-class ASTTranslationUnit : ASTBase {
+class ASTTranslationUnit : public ASTBase {
 };
 
 #endif
