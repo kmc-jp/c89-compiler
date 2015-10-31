07b2a5a7648c6c0e93622474a77f88a779425e0f
hatsusato <knightspm5@gmail.com>
2015-10-31 14:48:05 +0900

    Add VectorVectorIntTest test fixture

--- tests/vector_test/vector_vector_int_test.cpp
+++ tests/vector_test/vector_vector_int_test.cpp
@@ -6,3 +6,12 @@ extern "C" {
 #define VINT VECTORREF(int)
 #define VVINT VECTORREF(VINT)
 #define VVINTF(function) VECTORFUNC(VINT, function)
+
+class VectorVectorIntTest : public ::testing::Test {
+ protected:
+  VectorVectorIntTest() : v(VVINTF(ctor)()) {}
+  ~VectorVectorIntTest() {
+    VVINTF(dtor)(&v);
+  }
+  VVINT v;
+};
