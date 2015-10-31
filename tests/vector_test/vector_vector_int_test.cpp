6e3f44255ca4087f29e7bc3c5b387c29c7920613
hatsusato <knightspm5@gmail.com>
2015-10-31 14:45:28 +0900

    Add convenient macros

--- tests/vector_test/vector_vector_int_test.cpp
+++ tests/vector_test/vector_vector_int_test.cpp
@@ -2,3 +2,7 @@ extern "C" {
 #include "vector_vector_int.h"
 }
 #include "gtest/gtest.h"
+
+#define VINT VECTORREF(int)
+#define VVINT VECTORREF(VINT)
+#define VVINTF(function) VECTORFUNC(VINT, function)
