45301ba4154e384934c36d45ff93666bd153f0e7
hatsusato <knightspm5@gmail.com>
2015-11-03 15:24:01 +0900

    Rename string_entity to String
    CamelCased struct name

--- src/stdstring.h
+++ src/stdstring.h
@@ -1,12 +1,12 @@
 #ifndef KMC_C89_COMPILER_STDSTRING_H
 #define KMC_C89_COMPILER_STDSTRING_H
 
-struct string_entity {
+struct String {
   char* dat;
   size_t leng;
   size_t cap;
 };
 
-typedef struct string_entity* StringRef;
+typedef struct String* StringRef;
 
 #endif  /* KMC_C89_COMPILER_STDSTRING_H */
