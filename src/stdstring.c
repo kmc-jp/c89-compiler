1dd17d27df039822a2afd7e4a98ac2d28f6da945
Hideaki Nagamine <1995.hnagamin@gmail.com>
2015-11-03 13:25:00 +0900

    add stdstring functions

--- src/stdstring.c
+++ src/stdstring.c
@@ -1,1 +1,52 @@
+#include <stddef.h>
+#include <string.h>
 #include "stdstring.h"
+#include "utility.h"
+
+size_t string_length(string str) {
+  return str->leng;
+}
+
+size_t string_capacity(string str) {
+  return str->cap;
+}
+
+char *string_data(string str) {
+  return str->dat;
+}
+
+size_t smin(size_t a, size_t b) {
+  return (a < b ? a:b);
+}
+
+void string_resize(string str, size_t leng) {
+  size_t cap = leng + 1;
+  size_t end = smin(str->leng, leng);
+  char *dat = safe_malloc(sizeof(char) * cap);
+  strncpy(dat, str->dat, end);
+  dat[end] = '\0';
+  
+  str->dat = dat;
+  str->leng = end;
+  str->cap = cap;
+}
+
+size_t string_copy(string target, string src, size_t leng, size_t idx) {
+  leng = smin(leng, string_length(src));
+  string_resize(target, leng);
+  strncpy(string_data(target), string_data(src) + idx, leng);
+}
+
+void string_append(string bef, string aft) {
+  size_t oldleng = string_length(bef);
+  size_t leng = string_length(bef) + string_length(aft);
+  char *dat;
+  string_resize(bef, leng);
+  dat = string_data(bef);
+  strcpy(dat + oldleng, string_data(aft));
+}
+
+char* string_at(string str, int idx) {
+  return string_data(str) + idx;
+}
+
--- src/stdstring.h
+++ src/stdstring.h
@@ -1,3 +1,12 @@
 #ifndef KMC_C89_COMPILER_STDSTRING_H
 #define KMC_C89_COMPILER_STDSTRING_H
+
+struct string_entity {
+  char* dat;
+  size_t leng;
+  size_t cap;
+};
+
+typedef struct string_entity* string;
+
 #endif  /* KMC_C89_COMPILER_STDSTRING_H */
