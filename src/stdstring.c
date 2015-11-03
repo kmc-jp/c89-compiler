dfc339166d9fc2801c0b8c41aa80f8d256e4bea6
hatsusato <knightspm5@gmail.com>
2015-11-03 15:32:06 +0900

    Rename first argument of String member functions
    Describe owner object explicitly

--- src/stdstring.c
+++ src/stdstring.c
@@ -2,49 +2,49 @@
 #include "stdstring.h"
 #include "utility.h"
 
-size_t string_length(StringRef str) {
-  return str->length_;
+size_t string_length(StringRef self) {
+  return self->length_;
 }
 
-size_t string_capacity(StringRef str) {
-  return str->capacity_;
+size_t string_capacity(StringRef self) {
+  return self->capacity_;
 }
 
-char *string_data(StringRef str) {
-  return str->data_;
+char *string_data(StringRef self) {
+  return self->data_;
 }
 
 size_t smin(size_t a, size_t b) {
   return (a < b ? a:b);
 }
 
-void string_resize(StringRef str, size_t leng) {
+void string_resize(StringRef self, size_t leng) {
   size_t cap = leng + 1;
-  size_t end = smin(str->length_, leng);
+  size_t end = smin(self->length_, leng);
   char *dat = safe_malloc(sizeof(char) * cap);
-  strncpy(dat, str->data_, end);
+  strncpy(dat, self->data_, end);
   dat[end] = '\0';
 
-  str->data_ = dat;
-  str->length_ = end;
-  str->capacity_ = cap;
+  self->data_ = dat;
+  self->length_ = end;
+  self->capacity_ = cap;
 }
 
-size_t string_copy(StringRef target, StringRef src, size_t leng, size_t idx) {
+size_t string_copy(StringRef self, StringRef src, size_t leng, size_t idx) {
   leng = smin(leng, string_length(src));
-  string_resize(target, leng);
-  strncpy(string_data(target), string_data(src) + idx, leng);
+  string_resize(self, leng);
+  strncpy(string_data(self), string_data(src) + idx, leng);
 }
 
-void string_append(StringRef bef, StringRef aft) {
-  size_t oldleng = string_length(bef);
-  size_t leng = string_length(bef) + string_length(aft);
+void string_append(StringRef self, StringRef aft) {
+  size_t oldleng = string_length(self);
+  size_t leng = string_length(self) + string_length(aft);
   char *dat;
-  string_resize(bef, leng);
-  dat = string_data(bef);
+  string_resize(self, leng);
+  dat = string_data(self);
   strcpy(dat + oldleng, string_data(aft));
 }
 
-char* string_at(StringRef str, int idx) {
-  return string_data(str) + idx;
+char* string_at(StringRef self, int idx) {
+  return string_data(self) + idx;
 }
