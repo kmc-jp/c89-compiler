8f850d4f4bee2a23f498605255b609c4edf99716
Hideaki Nagamine <1995.hnagamin@gmail.com>
2015-11-03 17:47:07 +0900

    add prototype in stdstring.h

--- src/stdstring.h
+++ src/stdstring.h
@@ -9,4 +9,11 @@ struct String {
 
 typedef struct String* StringRef;
 
+size_t string_length(StringRef self);
+size_t string_capacity(StringRef self);
+char *string_data(StringRef self);
+void string_reserve(StringRef self, size_t length);
+size_t string_copy(StringRef self, StringRef src);
+void string_append(StringRef self, StringRef after);
+char string_at(StringRef self, size_t index);
 #endif  /* KMC_C89_COMPILER_STDSTRING_H */
