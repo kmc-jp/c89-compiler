5d9c49d0a75629d5b8fdf5a851d0cdfb5e356e5d
tyage <namatyage@gmail.com>
2015-11-09 18:24:54 +0900

    declaration_statement_list

---
+++ src/ast/declaration_statement_list.hpp
@@ -0,0 +1,7 @@
+#ifndef KMC_C89_COMPILER_AST_DECLARATION_STATEMENT_LIST_HPP
+#define KMC_C89_COMPILER_AST_DECLARATION_STATEMENT_LIST_HPP
+
+class ASTDeclarationStatementList : public ASTBase {
+};
+
+#endif
