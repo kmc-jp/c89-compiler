3ea10807a8b5983cd3c88c0118579d4f70cd5ef4
tyage <namatyage@gmail.com>
2015-11-09 20:45:33 +0900

    hoge_statement

---
+++ src/ast/arugment_list.hpp
@@ -0,0 +1,7 @@
+#ifndef KMC_C89_COMPILER_AST_ARUGMENT_LIST_HPP
+#define KMC_C89_COMPILER_AST_ARUGMENT_LIST_HPP
+
+class ASTArugmentList : public ASTBase {
+};
+
+#endif
---
+++ src/ast/for_statement.hpp
@@ -0,0 +1,7 @@
+#ifndef KMC_C89_COMPILER_AST_FOR_STATEMENT_HPP
+#define KMC_C89_COMPILER_AST_FOR_STATEMENT_HPP
+
+class ASTForStatement : public ASTBase {
+};
+
+#endif
