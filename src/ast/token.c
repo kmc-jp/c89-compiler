ffbb88fd5e3b2a6d420dbad28f81813a696c3b26
hatsusato <knightspm5@gmail.com>
2015-12-12 01:32:45 +0900

    Add make_identifier

--- src/ast/token.c
+++ src/ast/token.c
@@ -1,6 +1,16 @@
 #include "token.h"
+#include <assert.h>
+#include "../ast_pool.h"
 #include "../stdstring.h"
 
 struct Identifier {
   StringRef string;
 };
+
+IdentifierRef make_identifier(const char* src, size_t length) {
+  IdentifierRef self = NULL;
+  assert(src);
+  self = ast_palloc(struct Identifier, 1);
+  self->string = ast_make_string(src, length);
+  return self;
+}
--- src/ast/token.h
+++ src/ast/token.h
@@ -1,6 +1,10 @@
 #ifndef KMC_C89_COMPILER_TOKEN_H
 #define KMC_C89_COMPILER_TOKEN_H
 
+#include <stddef.h>
+
 typedef struct Identifier* IdentifierRef;
+
+IdentifierRef make_identifier(const char* src, size_t length);
 
 #endif  /* KMC_C89_COMPILER_TOKEN_H */
