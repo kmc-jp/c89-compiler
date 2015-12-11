bc8a36420cc56e2739c4c3f5d72c4916958907d4
hatsusato <knightspm5@gmail.com>
2015-12-11 23:51:09 +0900

    Add initializer and finalizer for AST pool

--- src/ast_pool.c
+++ src/ast_pool.c
@@ -2,6 +2,16 @@
 
 static MemoryPoolRef g_ast_pool = NULL;
 
+void initialize_ast_pool(size_t size) {
+  if (!g_ast_pool) {
+    g_ast_pool = memory_pool_ctor(size);
+  }
+}
+
+void finalize_ast_pool(void) {
+  memory_pool_dtor(&g_ast_pool);
+}
+
 MemoryPoolRef ast_pool(void) {
   return g_ast_pool;
 }
--- src/ast_pool.h
+++ src/ast_pool.h
@@ -3,6 +3,10 @@
 
 #include "memory_pool.h"
 
+void initialize_ast_pool(size_t size);
+
+void finalize_ast_pool(void);
+
 MemoryPoolRef ast_pool(void);
 
 #endif  /* KMC_C89_COMPILER_AST_POOL_H */
