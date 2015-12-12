fff191e91f7a41da5d3b86afc1cef17f7705b173
hatsusato <knightspm5@gmail.com>
2015-12-12 15:45:10 +0900

    Add ast_ctor

--- src/ast.c
+++ src/ast.c
@@ -1,8 +1,16 @@
 #include "ast.h"
+#include "ast_pool.h"
 
 struct Ast {
   enum AstTag tag;
   union AstData {
-    char* token;
+    void* dummy;
   } data;
 };
+
+static AstRef ast_ctor(void) {
+  AstRef ast = ast_palloc(struct Ast, 1);
+  ast->tag = AST_TAG_ENUM_END;
+  ast->data.dummy = NULL;
+  return ast;
+}
