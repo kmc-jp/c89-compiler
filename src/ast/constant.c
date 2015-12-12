918d8ef4f295f19837af5b465c7c7ac2cb9e2a11
hatsusato <knightspm5@gmail.com>
2015-12-12 18:49:13 +0900

    Add make_constant

--- src/ast/constant.c
+++ src/ast/constant.c
@@ -11,6 +11,18 @@ struct EnumerationConstant {
   IdentifierRef identifier;
 };
 
+ConstantRef make_constant(AstRef constant) {
+  ConstantRef self = NULL;
+  if (get_integer_constant(constant) ||
+      get_floating_constant(constant) ||
+      get_enumeration_constant(constant) ||
+      get_character_constant(constant)) {
+    self = ast_palloc(struct Constant, 1);
+    self->constant = constant;
+  }
+  return self;
+}
+
 EnumerationConstantRef make_enumeration_constant(AstRef identifier) {
   EnumerationConstantRef self = NULL;
   IdentifierRef data = get_identifier(identifier);
--- src/ast/constant.h
+++ src/ast/constant.h
@@ -6,6 +6,7 @@
 typedef struct Constant* ConstantRef;
 typedef struct EnumerationConstant* EnumerationConstantRef;
 
+ConstantRef make_constant(AstRef constant);
 EnumerationConstantRef make_enumeration_constant(AstRef identifier);
 
 #endif  /* KMC_C89_COMPILER_CONSTANT_H */
