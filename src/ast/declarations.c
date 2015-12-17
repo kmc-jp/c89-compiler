832d5bd3eb26c20b41e836f71f2288f61e202ed8
prime <prime@kmc.gr.jp>
2015-12-17 16:56:25 +0900

    implement ast_make_declaration_specifier

--- src/ast/declarations.c
+++ src/ast/declarations.c
@@ -133,3 +133,17 @@ AstRef ast_make_declaration_specifier_list(AstRef declaration_specifier,
   }
   return self;
 }
+
+AstRef ast_make_declaration_specifier(AstRef declaration_specifier) {
+  AstRef self = NULL;
+  if (ast_is_storage_class_specifier(declaration_specifier) ||
+      ast_is_type_specifier(declaration_specifier) ||
+      ast_is_type_qualifier(declaration_specifier)) {
+    AstDeclarationSpecifierRef data = ast_palloc(struct AstDeclarationSpecifier);
+    data->declaration_specifier = declaration_specifier;
+    self = ast_palloc(struct Ast);
+    self->tag = AST_DECLARATION_SPECIFIER;
+    self->data.declaration_specifier = data;
+  }
+  return self;
+}
