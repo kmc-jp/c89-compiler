5914f5542371d6f10f3fc8d7e628d3eab62abebb
hatsusato <knightspm5@gmail.com>
2016-09-02 20:49:39 +0900

    Add struct Sexpr

--- src/sexpr.h
+++ src/sexpr.h
@@ -1,4 +1,22 @@
 #ifndef KMC_C90_COMPILER_SEXPR_H
 #define KMC_C90_COMPILER_SEXPR_H
 
+enum SexprTag {
+  SEXPR_CONS
+};
+
+struct Sexpr;
+
+struct ConsData {
+  struct Sexpr* car;
+  struct Sexpr* cdr;
+};
+
+struct Sexpr {
+  enum SexprTag tag;
+  union Data {
+    struct ConsData cons;
+  } data;
+};
+
 #endif  /* KMC_C90_COMPILER_SEXPR_H */
