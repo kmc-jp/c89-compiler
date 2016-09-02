bab0c7dc0a4aecb317647144d2a9bcd848f00c6d
hatsusato <knightspm5@gmail.com>
2016-09-02 22:45:40 +0900

    Add IntegerData member

--- src/sexpr.h
+++ src/sexpr.h
@@ -2,7 +2,7 @@
 #define KMC_C90_COMPILER_SEXPR_H
 
 enum SexprTag {
-  SEXPR_CONS
+  SEXPR_CONS, SEXPR_INTEGER
 };
 
 struct Sexpr;
@@ -12,10 +12,13 @@ struct ConsData {
   struct Sexpr* cdr;
 };
 
+typedef long long IntegerData;
+
 struct Sexpr {
   enum SexprTag tag;
   union Data {
     struct ConsData cons;
+    IntegerData integer;
   } data;
 };
 
