1a972a5bc863a8d4380fafa0191e6c843214ddc1
hatsusato <knightspm5@gmail.com>
2016-09-02 23:25:00 +0900

    Add sexpr_pool

--- src/sexpr_pool.c
+++ src/sexpr_pool.c
@@ -5,6 +5,11 @@
 static const size_t SEXPR_POOL_CHUNK_SIZE = 1024;
 static MemoryPoolRef g_sexpr_pool = NULL;
 
+MemoryPoolRef sexpr_pool(void) {
+  assert(g_sexpr_pool);
+  return g_sexpr_pool;
+}
+
 void sexpr_initialize_pool(void) {
   assert(!g_sexpr_pool);
   g_sexpr_pool = memory_pool_ctor(SEXPR_POOL_CHUNK_SIZE);
